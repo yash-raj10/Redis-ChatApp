39aa10119968381a18a699908ab5adc9997d743a
yash <yashraj.se10@gmail.com>
2024-01-19 21:54:08 +0530

    Login Auth Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+import Button from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import Image from "next/image";
 
 export default async function Home() {
-  return (
-    <h1 className="text-3xl font-bold text-blue-500 underline">Hello world!</h1>
-  );
+  return <Button variant="ghost">Hello</Button>;
 }
