a1987a8529e71599b0d1e607f09df0b1c5557149
yash <yashraj.se10@gmail.com>
2024-01-19 13:44:07 +0530

    button done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
+import Button from "../components/ui/Button";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+  return <Button>hlo</Button>;
 };
 
 export default page;
